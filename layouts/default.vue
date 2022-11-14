27be3a0ffc7554bf4cc484bf22f28d6ecb446503
Terry Amagboro <rukkiecodes@gmail.com>
2022-11-14 13:40:10 +0100

    added nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,92 +1,152 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+    <v-app-bar
       app
+      flat
+      dense
+      fixed
+      color="transparent"
+      class="app-bar"
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-app-bar-nav-icon class="hidden-sm-and-up"></v-app-bar-nav-icon>
+
+      <v-app-bar-title class="logo mr-5">Dasy's fashion house</v-app-bar-title>
+
+      <nuxt-link
+        to="/"
+        class="text-decoration-none grey--text text--darken-4 hidden-sm-and-down"
+        active-class="black--text font-weight-bold"
+      >Women</nuxt-link>
+      <nuxt-link
+        to="/men"
+        class="text-decoration-none grey--text text--darken-4 mx-4 hidden-sm-and-down"
+        active-class="black--text font-weight-bold"
+      >Men</nuxt-link>
+      <nuxt-link
+        to="/kids"
+        class="text-decoration-none grey--text text--darken-4 hidden-sm-and-down"
+        active-class="black--text font-weight-bold"
+      >Kids</nuxt-link>
+
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          align-with-title
+          class="ml-1 hidden-sm-and-down"
+        >
+          <v-menu
+            offset-y
+            open-on-hover
+            v-for="(link, i) in links"
+            :key="i"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                dark
+                depressed
+                v-on="on"
+                v-bind="attrs"
+                color="transparent"
+                class="text-capitalize text-body-2"
+                :class="link.color || 'grey--text text--darken-4'"
+              >
+                {{link.title}}
+              </v-btn>
+            </template>
+
+            <v-card
+              min-width="100"
+              max-width="100%"
+            >
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="3"
+                  v-for="(subItem, i) in link.sublinks"
+                  :key="i"
+                >
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-list-item-title class="mb-4">{{subItem.title}}</v-list-item-title>
+                        <v-list-item-subtitle
+                          v-for="(childLink, i) in subItem.sublinks"
+                          :key="i"
+                          class="mt-3"
+                        >
+                          <nuxt-link
+                            class="text-decoration-none grey--text text--darken-3"
+                            :to="childLink.to"
+                          >{{ childLink.title }}</nuxt-link>
+                        </v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-col>
+              </v-row>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </v-tabs>
+
+        <v-spacer />
+        <v-text-field
+          placeholder="Search"
+          hide-details
+          outlined
+          dense
+          append-icon="mdi-magnify"
+          class="hidden-sm-and-down"
+        />
+      </template>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import links from './links'
 export default {
   name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
+  data: () => ({
+    fav: true,
+    menu: false,
+    message: false,
+    hints: true,
+    links
+  })
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+.logo {
+  font-family: 'Pacifico', cursive !important;
+}
+
+.app-bar {
+  background: rgba(255, 255, 255, 0);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+}
+</style>
---
+++ layouts/links.js
@@ -0,0 +1,374 @@
+export default [
+  {
+    title: 'Gifts',
+    sublinks: [
+      {
+        title: 'GIFTS BY STYLE',
+        sublinks: [
+          { title: 'Classic', to: '/claccic' },
+          { title: 'Glamore', to: '/glamore' },
+          { title: 'Modern feminity', to: '/modernFeminity' },
+          { title: 'Sweater', to: '/sweater' },
+        ]
+      },
+      {
+        title: 'GIFTS BY CATEGORY',
+        sublinks: [
+          { title: "The season's most wanted", to: '/seasonsMostWanted' },
+          { title: 'Gifts that do good', to: '/giftsThatDoGood' },
+          { title: 'Homeware to love', to: '/homeWareToLove' },
+          { title: 'Logo accessoris', to: '/Logo accessories' },
+          { title: '99 hype sneakers', to: '/99hypeSneakers' },
+          { title: 'Fine jewellery & watches', to: '/fineJewelleryAndWatch' },
+          { title: 'Top 100 iconinc bags', to: '/top100IconicBags' },
+        ]
+      },
+      {
+        title: 'GIFTS BY DESIGNER',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      },
+    ]
+  },
+  {
+    title: 'New In',
+    sublinks: [
+      {
+        title: "WHAT'S NEW",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'TRENDING NOW',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      },
+      {
+        title: 'DISCOVER',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      },
+      {
+        title: 'THE HEIGHLIGHTS',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Brands',
+    sublinks: [
+      {
+        title: "FEATURED BRANDS",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Clothing',
+    sublinks: [
+      {
+        title: "CLOTHING",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'FEATURED BRANDS',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      },
+      {
+        title: 'DISCOVER',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      },
+      {
+        title: 'DONATE & REPARE',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Shoes',
+    sublinks: [
+      {
+        title: "SHOES",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'FEATURED BRANDS',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      },
+      {
+        title: 'DONATE & REPARE',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Bags',
+    sublinks: [
+      {
+        title: "BAGS",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'ICONIC COLLECTIONS',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      },
+      {
+        title: 'SELL, DONATE, REPARE',
+        sublinks: [
+          { title: "Alexander McQueen", to: '/alexanderMcQueen' },
+          { title: 'Balenciaga', to: '/balenciaga' },
+          { title: 'Burberry', to: '/burberry' },
+          { title: 'Dolce & Gabbana', to: '/dolceAndGabbana' },
+          { title: 'Gucci', to: '/gucci' },
+          { title: 'Jacquemus', to: '/jacquemus' },
+          { title: 'Off-White', to: '/offWhite' },
+          { title: 'Saint Laurent', to: '/saintLaurent' },
+          { title: 'Ferragamo', to: '/ferragamo' },
+          { title: 'Valentino Garavani', to: '/valentinoGaravani' },
+          { title: 'Versace', to: '/versace' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Accessories',
+    sublinks: [
+      {
+        title: "ACCESSORIES",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'HOMEWARE',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Jewelry',
+    sublinks: [
+      {
+        title: "ACCESSORIES",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'HOMEWARE',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Pre-owned',
+    sublinks: [
+      {
+        title: "ACCESSORIES",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'HOMEWARE',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Sale',
+    color: 'red--text text--accent-4',
+    sublinks: [
+      {
+        title: "ACCESSORIES",
+        sublinks: [
+          { title: 'New in today', to: '/' },
+          { title: 'New: claccic', to: '/' },
+          { title: 'New: expressionist', to: '/' },
+          { title: 'New: glamour', to: '/' },
+          { title: 'New: hypw swaeatwear', to: '/' },
+          { title: 'New: minimalism', to: '/' },
+          { title: 'New: modern feminity', to: '/' },
+        ]
+      },
+      {
+        title: 'HOMEWARE',
+        sublinks: [
+          { title: 'The party edit', to: '/seasonsMostWanted' },
+          { title: 'New-season skiwear', to: '/giftsThatDoGood' },
+          { title: 'The puffer bag', to: '/homeWareToLove' },
+          { title: '70 oversized coats', to: '/Logo accessories' },
+          { title: 'The Brons Fashion edit', to: '/99hypeSneakers' },
+        ]
+      }
+    ]
+  },
+]
