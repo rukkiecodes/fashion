ef56c33086fa91aa95b0510761590d7792afe049
rukkiecodes <rukkiecodes@gmail.com>
2022-11-28 11:49:22 +0100

    our services

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - fashion',
-    title: 'fashion',
+    titleTemplate: '',
+    title: 'Wanlainjo merch',
     htmlAttrs: {
       lang: 'en',
     },
